63b3704dab01ea8a408bd613d3710f5ec3786f39
maxonemillion <max.daniel@utah.edu>
2020-11-14 22:31:29 -0700

    small change

--- routes/api.js
+++ routes/api.js
@@ -13,6 +13,17 @@ router.post("/api/workouts", ({ body }, res) => {
 })
 
 
+router.get("/api/workouts/", (req, res) => {
+    Workout.find({})
+    .sort({ date: -1 })
+    .then(dbWorkout => {
+        res.json(dbWorkout)
+    })
+    .catch(err => {
+        res.status(400).json(err)
+    });
+});
+
 router.get("/api/workouts/range", (req, res) => {
     Workout.find({})
     .sort({ date: -1 })
